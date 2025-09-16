abe199dd20ece5b167789eb975581f68aa53cf7a
Nithish Deepala <119877777+Nithish9652@users.noreply.github.com>
2025-09-16 23:09:08 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
-=======
 module.exports = { reactStrictMode: true }
->>>>>>> ca826c0a90c60775cf0712d0b34d2388aed08405
